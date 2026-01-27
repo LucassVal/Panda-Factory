943abdb4e74e013e4e9babcf99136cc6a34bc5a2
Lucas Valério <lucassvalerio@gmail.com>
2026-01-27 09:41:08 -0300

    [FIX] Correct redirect path and vite base for local testing

--- jam/vite.config.js
+++ jam/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/jam/",
+  base: "./",
   build: {
     outDir: "../dist/jam",
     emptyOutDir: true,
